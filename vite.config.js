d4d851be5784627a55d5c9be15cfe2371bc9a377
KaloudasDev <157753309+KaloudasDev@users.noreply.github.com>
2026-02-19 22:31:54 +0200

    Minor testing update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pontus-historical-map/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false
+  },
+  server: {
+    port: 5173,
+    open: true
+  }
 })
